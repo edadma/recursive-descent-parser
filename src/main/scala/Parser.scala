d8782d9b5224dc59cef1037e226d56ed89971cc2
Edward A Maxedon, Sr <edadma@gmail.com>
2019-01-23 14:19:05 -0500

    start coding left associative rule

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -3,18 +3,23 @@ package xyz.hyperreal.recursive_descent_parser
 import xyz.hyperreal.pattern_matcher.{Matchers, Reader}
 
 
+abstract class Token {
+  val pos: Reader
+  val value: String
+}
+
+case class IdentToken( pos: Reader, value: String ) extends Token
+case class SymbolToken( pos: Reader, value: String ) extends Token
+case class IntegerToken( pos: Reader, value: String ) extends Token
+case class ErrorToken( pos: Reader, value: String ) extends Token
+case class EOIToken( pos: Reader ) extends Token { val value = null }
+
+trait Result
+
+case class Failure( msg: String, rest: Stream[Token] ) extends Result
+case class Success( rest: Stream[Token], result: AST ) extends Result
+
 class Parser {
-
-  abstract class Token {
-    val pos: Reader
-    val value: String
-  }
-
-  case class IdentToken( pos: Reader, value: String ) extends Token
-  case class SymbolToken( pos: Reader, value: String ) extends Token
-  case class IntegerToken( pos: Reader, value: String ) extends Token
-  case class ErrorToken( pos: Reader, value: String ) extends Token
-  case class EOIToken( pos: Reader ) extends Token { val value = null }
 
   object Lexer extends Matchers[Reader] {
 
@@ -49,11 +54,6 @@ class Parser {
 
   }
 
-  trait Result
-
-  case class Failure( msg: String, rest: Stream[Token] ) extends Result
-  case class Success( rest: Stream[Token], result: AST ) extends Result
-
   def tokenStream( r: Reader ): Stream[Token] = {
     Lexer.token( r ) match {
       case m: Lexer.Mismatch => m.error
@@ -63,54 +63,61 @@ class Parser {
 
   def apply( r: Reader, rule: Rule ) = rule( tokenStream(r) )
 
-  abstract class Rule {
+}
 
-    def apply( t: Stream[Token] ): Result
+abstract class Rule {
 
-  }
+  def apply( t: Stream[Token] ): Result
 
-  class Alternates( rs: List[Rule] ) extends Rule {
+}
 
-    require( rs nonEmpty, "there must be at least one alternate" )
+class Alternates( rs: List[Rule] ) extends Rule {
 
-    def apply( t: Stream[Token] ): Result = {
-      def alternates( alts: List[Rule] ): Result =
-        alts match {
-          case List( r ) => r( t )
-          case hd :: tl =>
-            hd( t ) match {
-              case _: Failure => alternates( tl )
-              case s: Success => s
-            }
-        }
+  require( rs nonEmpty, "there must be at least one alternate" )
 
-      alternates( rs )
-    }
-
-  }
-
-  object IntegerRule extends Rule {
-
-    def apply( t: Stream[Token] ) =
-      t.head match {
-        case IntegerToken( pos, value ) => Success( t.tail, IntegerAST(pos, value.toInt) )
-        case _ => Failure( "expected integer", t )
+  def apply( t: Stream[Token] ): Result = {
+    def alternates( alts: List[Rule] ): Result =
+      alts match {
+        case List( r ) => r( t )
+        case hd :: tl =>
+          hd( t ) match {
+            case _: Failure => alternates( tl )
+            case s: Success => s
+          }
       }
+
+    alternates( rs )
   }
 
-//class LeftAssocBinary( p: Parser, ops: Set[String] ) extends Parser {
-//
-//  def apply( r: Reader ): Result =
-//
-//  def expr( r: Result, left: AST ): Result =
-//    p( r ) match {
-//      case f: Failure => f
-//      case s: Success =>
-//
-//    }
-//
-//
-//}
+}
 
+class LeftAssocBinary( expr: Rule, ops: Set[String] ) extends Rule {
 
+  def apply( t: Stream[Token] ): Result = {
+    var res: AST = null
+
+    def parse_expr( rest: Stream[Token], ast: AST ) =
+      expr( t ) match {
+        case f: Failure => f
+        case s: Success =>
+          val tok = rest.head
+
+          if (ops contains tok.value) {
+
+          } else
+            s
+      }
+
+    parse_expr( t, null )
+  }
+
+}
+
+object IntegerRule extends Rule {
+
+  def apply( t: Stream[Token] ) =
+    t.head match {
+      case IntegerToken( pos, value ) => Success( t.tail, IntegerAST(pos, value.toInt) )
+      case _ => Failure( "expected integer", t )
+    }
 }
